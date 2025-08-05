2ff612c37662fbf11d189835396c58d2ffef313f
YordanovTsanko <faxyhitz1@gmail.com>
2025-08-05 17:33:17 +0300

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,9 @@ import { ReactComponent as HearthIcon } from "../assets/hearth.svg";
 import { ReactComponent as ProfileIcon } from "../assets/profile.svg";
 
 const navLinks = [
-  { name: "НАЧАЛО", path: "/home", bgColor: "bg-primary" },
-  { name: "УСЛУГИ", path: "/about", bgColor: "bg-primary" },
-  { name: "БЛОГ", path: "/contact", bgColor: "bg-secondary" },
+  { name: "НАЧАЛО", path: "/", bgColor: "bg-primary", hover: "hover:bg-primary/90" },
+  { name: "УСЛУГИ", path: "/about", bgColor: "bg-primary", hover: "hover:bg-primary/90" },
+  { name: "БЛОГ", path: "/contact", bgColor: "bg-secondary", hover: "hover:bg-secondary/90" },
 ];
 
 const NavBar = () => {
@@ -23,20 +23,33 @@ const NavBar = () => {
           <Link
             key={link.name}
             to={link.path}
-            className={`${link.bgColor} text-white text-md py-1 px-6 rounded-lg font-medium`}
+            className={`${link.bgColor + " " + link.hover} shadow-lg transition text-white text-md py-1 px-6 rounded-lg font-medium`}
           >
             {link.name}
           </Link>
         ))}
       </div>
 
-      <div className="flex items-center space-x-6">
-        <Link to="/favorites">
-          <HearthIcon className="h-6 w-6 text-primary" />
-        </Link>
-        <Link to="/profile">
-          <ProfileIcon className="h-6 w-6 text-primary" />
-        </Link>
+      <div className="flex items-center gap-8">
+        <div className="flex items-center space-x-2">
+          <Link to="/favorites">
+            <HearthIcon className="h-7 w-7" />
+          </Link>
+          <div className="flex items-end cursor-pointer">
+            <ProfileIcon className="h-7 w-7" />
+            <span
+              className="inline-block text-primary ms-[7px]"
+              style={{
+                fontSize: "1rem",
+                lineHeight: "1",
+                transform: "rotate(90deg) scaleX(1.2) scaleY(2)",
+                transformOrigin: "center",
+              }}
+            >
+              {">"}
+            </span>
+          </div>
+        </div>
         <Link
           to="/"
           className="bg-primary text-white font-semibold py-2 px-2 rounded-3xl shadow hover:bg-primary/90 transition"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='bg-primary'>
+    <div className='bg-primary min-h-screen w-full'>
       
     </div>
   )
